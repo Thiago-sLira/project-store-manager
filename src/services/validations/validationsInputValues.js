2b8c4f383fceefbd2d59a397baff610bf7e4c75b
Thiago Lira <thiago.slira@outlook.com>
2023-03-21 18:38:33 -0300

    Ajustes no errorMap e validations

--- src/services/validations/validationsInputValues.js
+++ src/services/validations/validationsInputValues.js
@@ -1,7 +1,7 @@
-const { idSchema } = require('./schema');
+const schema = require('./schema');
 
 const validateId = (id) => {
-  const { error } = idSchema.validate(id);
+  const { error } = schema.idSchema.validate(id);
   if (error) return { type: 'INVALID_VALUE', message: '"id" must be a number' };
 
   return { type: null, message: '' };
--- src/utils/errorMap.js
+++ src/utils/errorMap.js
@@ -1,9 +1,9 @@
 const errorMap = {
   PRODUCT_NOT_FOUND: 404,
-  INVALID_VALUE: 422,
-  TRAVEL_NOT_FOUND: 404,
-  DRIVER_NOT_FOUND: 404,
-  TRAVEL_CONFLICT: 409,
+  // INVALID_VALUE: 422,
+  // TRAVEL_NOT_FOUND: 404,
+  // DRIVER_NOT_FOUND: 404,
+  // TRAVEL_CONFLICT: 409,
 };
 
 const mapError = (type) => errorMap[type] || 500;
