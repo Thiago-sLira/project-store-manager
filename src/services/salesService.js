6b21f549f2bd7d314460ecbce4a46136315a6a51
Thiago Lira <thiago.slira@outlook.com>
2023-03-22 15:51:56 -0300

    Criada camada de service com devidas validações de sales

--- src/services/salesService.js
+++ src/services/salesService.js
@@ -1,7 +1,21 @@
 const salesModel = require('../models/salesModel');
+const { mapError } = require('../utils/errorMap');
+const errorMessage = require('../utils/errorMessage');
+const schema = require('./validations/validationsInputValues');
 
 const registerNewSale = async (sales) => { 
-  const 
+  const quantity = schema.validateQuantity(sales);
+  if (quantity.type) {
+    throw errorMessage(quantity.type, quantity.message);
+  }
+
+  const product = sales.some(({ productId }) => !salesModel.findProductByID(productId));
+  if (product) {
+    throw errorMessage(mapError('PRODUCT_NOT_FOUND'), 'Product not found');
+  }
+
+  const newSale = await salesModel.registerNewSale(sales);
+  return newSale;
 };
 
 module.exports = {
